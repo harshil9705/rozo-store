47673737ec27a47a941779350cd296e95eaa1f54
Harshil Lakhani <140501445+harshil9705@users.noreply.github.com>
2025-04-05 18:37:36 +0530

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require("mongoose");
 const connection = ()=>{
-    mongoose.connect("mongodb+srv://harshillakhani009:harshillakhani009@cluster0.pukbpzd.mongodb.net/?retryWrites=true&w=majority")
+    mongoose.connect(process.env.db_url)
     console.log("database connected");
 }
 
